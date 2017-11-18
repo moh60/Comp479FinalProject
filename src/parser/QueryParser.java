bb3d68a74b0137111ab3aa6691c8b7bebd1b0aac
EthanShen8189 <sy8189@gmail.com>
2017-11-18 18:13:10 -0500

    Added more sanity check on input string

--- src/parser/QueryParser.java
+++ src/parser/QueryParser.java
@@ -2,15 +2,24 @@ package parser;
 
 import java.util.ArrayList;
 
+
 public class QueryParser extends DocParser {
 
     private static ArrayList<String> tokenizeQuery(String unprocessedText){
         return tokenizeText(unprocessedText);
     }
 
-    public static ArrayList<String> preprocessQuery(String unprocessedText){
-        return preprocessTokens(tokenizeQuery(unprocessedText), SettingParser.readSettings());
+    public static String[] preprocessQuery(String unprocessedText){
+        if(unprocessedText != null && !unprocessedText.equals("")) {
+            ArrayList<String> processedQuery = preprocessTokens(tokenizeQuery(unprocessedText), SettingParser.readSettings());
+            StringBuilder stringBuilder = new StringBuilder();
+            for(String s: processedQuery){
+                stringBuilder.append(s);
+                stringBuilder.append(" ");
+            }
+            String raw = stringBuilder.toString();
+            return raw.split(" ");
+        }
+        return null;
     }
-
-
 }
